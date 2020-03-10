ce081e379715da3dbb1cd0c9515ad9a7f038faca
fadholifh <fadholifh14@gmail.com>
2020-03-10 17:04:11 +0700

    register done

--- index.js
+++ index.js
@@ -36,18 +36,53 @@ app.get('/', (req, res) => {
     res.send(`hello world`);
 });
 
-
+// register
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
 
-    user.save((err, userData) => {
+    user.save((err, doc) => {
         if (err) return res.json({
             success: err
         })
+        return res.status(200).json({
+            success:true,
+            userData: doc
+        })
     })
-    return res.status(200).json({
-        success:true
-    })
+});
+
+//login
+app.post("/api/user/login", (req, res) => {
+    //find email
+    User.findOne({
+        email: req.body.email
+    }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            })
+        }
+        //comparePassword
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "Wrong password"
+                });
+            }
+        });
+
+        //generateToken
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(200)
+                json({
+                    loginSuccess: true
+                })
+        })
+    });
 });
 
 const port = process.env.PORT || 3000;
--- models/user.js
+++ models/user.js
@@ -1,35 +1,77 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
     name: {
-        type:String,
-        maxlength:50
+        type: String,
+        maxlength: 50
     },
     email: {
-        type:String,
-        trim:true,
+        type: String,
+        trim: true,
         unique: 1
     },
     password: {
-        type:String,
-        minlength:5
+        type: String,
+        minlength: 5
     },
     lastname: {
-        type:String,
-        maxlength:50
+        type: String,
+        maxlength: 50
     },
     role: {
-        type:Number,
-        default:0
+        type: Number,
+        default: 0
     },
     token: {
-        type:String,
+        type: String
     },
     tokenExp: {
-        type:Number
+        type: Number
     }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", function(next) {
+    let user = this;
 
-module.exports = { User }
+    if (user.isModified("password")) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+//compare
+userSchema.methods.comparePassword = (plainPassword, cb) => {
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = cb => {
+    let user = this;
+    let token = jwt.sign(user._id.toHexString(), "fukfuk");
+
+    user.token;
+    user.save((err, user) => {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+};
